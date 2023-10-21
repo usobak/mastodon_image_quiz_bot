7cd9b77971d97372f9dcc43e0bcd87c1f3cfb0b5
usobak <you@example.com>
2023-10-21 10:14:37 +0100

    Updated bot strings

--- bot/strings.py
+++ bot/strings.py
@@ -6,5 +6,7 @@ NEW_CLUE = 'Pista {}/{} ¿Conoces este juego? 🤔 Escribe el título en los com
 
 LAST_CLUE = '¡Última oportunidad!\n' + NEW_CLUE + FOOTER
 
-SOLUTION_FOUND = '¡Alguien ha encontrado la solución! 😎\nEl título es: {}' + FOOTER
-SOLUTION_NOT_FOUND = '¡Qué pena! Nadie ha adivinado el título. 😩 \nEl juego es: {}' + FOOTER
+NEW_GAME_SOON = '¿Has jugado a este juego? ¿Qué te parece?\nEn unos minutos empezará una nueva ronda.' + FOOTER
+
+SOLUTION_FOUND = '¡Alguien ha encontrado la solución! 😎\nEl título es: {}\n' + NEW_GAME_SOON
+SOLUTION_NOT_FOUND = '¡Qué pena! Nadie ha adivinado el título. 😩 \nEl juego es: {}\n' + NEW_GAME_SOON
